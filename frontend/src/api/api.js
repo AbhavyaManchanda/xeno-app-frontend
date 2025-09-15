6f55d16ac0a54ad27a1f8555187897e53d5455f7
AbhavyaManchanda <87229986+AbhavyaManchanda@users.noreply.github.com>
2025-09-15 20:48:41 +0530

    scripts

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,5 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "http://localhost:4000",
 });
+const API = process.env.REACT_APP_API_URL;
+fetch(`${API}/api/items`).then();
 
 export default api;
---
+++ frontend/src/config/config.js
@@ -0,0 +1,1 @@
+export const API_URL = "https://xeno-app-backend.onrender.com";
